5a88bd9f927bf3da96c537fb6f04b08a84a9bf87
duy.nguyen <duy.nguyen@vnlp.ai>
2021-04-01 17:23:37 +0700

    update log to show order of running flow

--- main.go
+++ main.go
@@ -2,12 +2,38 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func crawlData(url string, channel chan string) {
-	time.Sleep(time.Second * 2)
+	fmt.Println("Running " + url)
+	randomTime := int(rand.Intn(5) + 2)
+	time.Sleep(time.Second * time.Duration(randomTime))
 	channel <- "Data get from " + url
+}
+
+func limitCrawl(limit int, listUrl []string) {
+	lengthUrl := len(listUrl)
+	if limit > lengthUrl {
+		limit = lengthUrl
+	}
+	channel := make(chan string, limit)
+	index := 0
+	for ; index < limit; index++ {
+		go crawlData(listUrl[index], channel)
+	}
+
+	for i := 0; i < lengthUrl; i++ {
+		select {
+		case response := <-channel:
+			fmt.Println(response)
+			if index < lengthUrl {
+				go crawlData(listUrl[index], channel)
+				index++
+			}
+		}
+	}
 }
 
 func main() {
@@ -25,22 +51,6 @@ func main() {
 		"vnlp.ai",
 	}
 
-	channel := make(chan string, 5)
-	index := 0
-	for ; index < 5; index++ {
-		go crawlData(listUrl[index], channel)
-	}
-
-	lengthUrl := len(listUrl)
-	for i := 0; i < lengthUrl; i++ {
-		select {
-		case response := <-channel:
-			fmt.Println(response)
-			if index < lengthUrl {
-				go crawlData(listUrl[index], channel)
-				index++
-			}
-		}
-	}
+	limitCrawl(6, listUrl)
 	fmt.Println("OK")
 }
